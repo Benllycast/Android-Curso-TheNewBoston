cc3aa16d8f8db69d19290ca9f4f6e0d48d39ec04
benllycast <benllycast@gmail.com>
2012-12-01 14:38:26 -0500

    puesta a punto del widget

--- src/com/example/ejemplo_adroid/PointlessWidget.java
+++ src/com/example/ejemplo_adroid/PointlessWidget.java
@@ -7,23 +7,33 @@ import android.appwidget.AppWidgetProvider;
 import android.content.Context;
 import android.widget.RemoteViews;
 import android.widget.Toast;
-
+/*
+ * esta clase define un widget que recive actualizaciones de broadcast del dispositivo
+ * un widget se implementa mediante la clase AppWidgetProvider y se define en el manifest por
+ * medio de un receiver
+ * 
+ * por defecto se define el metodo onUpdate y onDelete
+ */
 public class PointlessWidget extends AppWidgetProvider {
 
+	private String rand;
+	
+	//se llama cuando se elimina un widget
 	@Override
 	public void onDeleted(Context context, int[] appWidgetIds) {
 		// TODO Auto-generated method stub
 		super.onDeleted(context, appWidgetIds);
-		Toast.makeText(context, "See ya suckaa", Toast.LENGTH_SHORT).show();
+		Toast.makeText(context, "See ya suckaa"+rand, Toast.LENGTH_SHORT).show();
 	}
 
+	//se llama cada cierto tiempo definido en widget_stuff
 	@Override
 	public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
 		// TODO Auto-generated method stub
 		super.onUpdate(context, appWidgetManager, appWidgetIds);
 		Random r = new Random();
 		int randomInt = r.nextInt(10000000);
-		String rand = String.valueOf(randomInt);
+		rand = String.valueOf(randomInt);
 		final int N = appWidgetIds.length;
 		for (int i = 0; i < N; i++) {
 			int awID = appWidgetIds[i];
@@ -32,6 +42,4 @@ public class PointlessWidget extends AppWidgetProvider {
 			appWidgetManager.updateAppWidget(awID, v);
 		}
 	}
-
-    
 }
--- src/com/example/ejemplo_adroid/WidgetConfig.java
+++ src/com/example/ejemplo_adroid/WidgetConfig.java
@@ -1,5 +1,81 @@
 package com.example.ejemplo_adroid;
 
-public class WidgetConfig {
+
+import android.app.Activity;
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RemoteViews;
+
+/*esta clase se llama solo una vez para toda instancia de un widget PointLessWidget
+ *  y usa cuando se agrega un Widget PointLessWidget la pantalla home del dispositivo
+ *  y configura la apariencia del view del widget por primera vez
+ *  
+ *   esta clase es la que se define en el widget_stuff
+ */
+public class WidgetConfig extends Activity implements OnClickListener{
+
+	private Button b;
+	private EditText info;
+	private Context c;	//contexto de la actividad
+	private int awID;	//ID del widget que lanza el llamado de la actividad
+	private AppWidgetManager awm;	//administrador de widget's
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.widgetconfig);
+		b = (Button) findViewById(R.id.bwidgetconfig);
+		b.setOnClickListener(this);
+		c = WidgetConfig.this;
+		info =(EditText) findViewById(R.id.etwidgetconfig);
+		//obtiniendo info desde el widget que lanzo esta Activity 
+		Intent i = getIntent();	//se obtiene el Intent del widget que lanzo la Activity
+		Bundle extras = i.getExtras();	//se obtinen los parametros extras desde el intent 
+		if(extras != null){
+			//si hay extras se rescata el ID del widget que lanzo la aplicaion, pueden haber varios widget hacia la misma aplicaion
+			awID = extras.getInt(AppWidgetManager.EXTRA_APPWIDGET_ID, AppWidgetManager.INVALID_APPWIDGET_ID);
+		}else{
+			//sino se termina la aplicaion
+			finish();
+		}
+		//se obtiene una instancia del adminsitrador de AppWidget
+		awm = AppWidgetManager.getInstance(c);
+
+	}
+	
+	public void onClick(View v) {
+		// TODO Auto-generated method stub
+		String e = info.getText().toString();
+		//se crea un objeto de RemoteView para Actualizar el Layout del Widget 
+		RemoteViews views = new RemoteViews(c.getPackageName(), R.layout.widget);
+		views.setTextViewText(R.id.tvConfigInput, e);	//actualiza el campo requerido
+		
+		Intent in = new Intent(c, Menu.class);	//nuevo Intent que se lanzara una aplicacion desde el widget
+		PendingIntent pi = PendingIntent.getActivity(c, 0, in, 0);	//????? es importante
+		//enlaza el evento on-click al boton para que lanze un pendingIntent
+		views.setOnClickPendingIntent(R.id.bwidgetOpen, pi);	
+		
+		//actualiza el Layout del widget con la nueva view
+		awm.updateAppWidget(awID, views);
+		
+		//nuevo intent que reporta el resultado de la actividad
+		Intent result = new Intent();
+		//se le pasa un extra con el ID del widget que lanzo al actividad
+		result.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, awID);
+		//se setea el resultado de la actividad en el objeto
+		setResult(RESULT_OK, result);
+		//se finaliza la actividad
+		finish();
+	}
+
+	
 
 }
